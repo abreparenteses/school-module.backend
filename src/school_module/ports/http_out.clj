c7d8c06e7b792d34d54dbf07c8b7a72ecc2519bb
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2024-11-25 21:39:12 -0300

    fix: removing unused imports

--- src/school_module/ports/http_out.clj
+++
@@ -1,5 +0,0 @@
-(ns school-module.ports.http-out
-  (:require [school-module.adapters :as adapters.price]
-            [school-module.schemas.types :as schemas.types]
-            [parenthesin.components.http.clj-http :as components.http]
-            [schema.core :as s]))
